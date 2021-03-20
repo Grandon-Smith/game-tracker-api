8353f270f3f83758b8a49bf36873d7bf774fb8b0
Grandon Smith <grandon.smith@yahoo.com>
2021-03-20 11:14:53 -0400

    errors?

--- src/app.js
+++ src/app.js
@@ -165,12 +165,15 @@ app.get('/', (req, res) => {
 app.use(function errorHandler(error, req, res, next) {
     let response;
     if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
+        response = { message: error.message,
+        other: 'error here'
+    }
     } else {
         console.error(error)
         response = { message: error.message, error }
     }
     res.status(500).json(response)
+    .catch(next)
 });
 
 module.exports = app;
