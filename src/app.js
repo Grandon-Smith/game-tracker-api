d47a70030bef7b7083bdabff8b3d765f302f4b91
Grandon Smith <grandon.smith@yahoo.com>
2021-03-20 11:30:46 -0400

    eureka??

--- src/app.js
+++ src/app.js
@@ -43,107 +43,107 @@ const db = knex({
   }
 })
 
-// app.delete('/removegame', jsonParser, (req, res, next) => {
-//     const knexInstance = req.app.get('db')
-//     const { email, gameid } = req.body;
-//     EndpointsService.removeUserGame(knexInstance, email, gameid)
-//     .then(numRowsAffected => {
-//         res.status(204).end()
-//     })
-//     .catch(next)
-// })
+app.delete('/removegame', jsonParser, (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    const { email, gameid } = req.body;
+    EndpointsService.removeUserGame(knexInstance, email, gameid)
+    .then(numRowsAffected => {
+        res.status(204).end()
+    })
+    .catch(next)
+})
 
-// app.post('/addgame', jsonParser, (req, res, next) => {
-//     const knexInstance = req.app.get('db')
-//     const { email, gameid } = req.body;
-//     const game = {email, gameid}
-//     EndpointsService.createUserGame(knexInstance, game)
-//         .then(game => {
-//             if(!game) {
-//                 console.log('error')
-//                 res.json({message: 'error'})
-//             }
-//             res.json({message: 'good'})
-//         })
-//         .catch(next)
-// })
+app.post('/addgame', jsonParser, (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    const { email, gameid } = req.body;
+    const game = {email, gameid}
+    EndpointsService.createUserGame(knexInstance, game)
+        .then(game => {
+            if(!game) {
+                console.log('error')
+                res.json({message: 'error'})
+            }
+            res.json({message: 'good'})
+        })
+        .catch(next)
+})
 
-// app.post('/usergames', jsonParser, (req, res, next) => {
-//     const knexInstance = req.app.get('db')
-//     const { email } = req.body
-//     console.log(email)
-//     EndpointsService.getUserGames(knexInstance, email)
-//         .then(games => {
-//             if(!games) {
-//                 res.status(204).json({
-//                     error: { message: `Uh oh. Your games are gone!` }
-//                 })
-//             }
-//             console.log(games)
-//             res.json(games)
-//         })
-//         .catch(next)
-// })
+app.post('/usergames', jsonParser, (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    const { email } = req.body
+    console.log(email)
+    EndpointsService.getUserGames(knexInstance, email)
+        .then(games => {
+            if(!games) {
+                res.status(204).json({
+                    error: { message: `Uh oh. Your games are gone!` }
+                })
+            }
+            console.log(games)
+            res.json(games)
+        })
+        .catch(next)
+})
 
-// app.post('/create-account', jsonParser, (req, res, next) => {
-//     const knexInstance = req.app.get('db')
-//     const { username, email, password } = req.body;
-//     let response = validator.validate(email.trim())
+app.post('/create-account', jsonParser, (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    const { username, email, password } = req.body;
+    let response = validator.validate(email.trim())
 
-//     if(response === true) {
-//         console.log('TRUE')
-//         const role = 'user'
-//         const newUser = {username, email, role, password};
-//         EndpointsService.createNewUser(knexInstance, newUser)
-//             .then(user => {
-//                 return res
-//                     .status(201)
-//                     .json(user)
-//             })
-//             .catch(next)
-//     } else if (response === false){
-//         console.log('FALSE')
-//         return res
-//             .status(200)
-//             .json({
-//                 errorMessage: "It looks like that isn\'t a valid email address, please check that it is correct and try again."
-//             })
-//     } else {
-//         console.log('ERROR')
-//         return res
-//             .status(500)
-//             .json({errorMessage: 'Internal error'})
-//     };
-// })
+    if(response === true) {
+        console.log('TRUE')
+        const role = 'user'
+        const newUser = {username, email, role, password};
+        EndpointsService.createNewUser(knexInstance, newUser)
+            .then(user => {
+                return res
+                    .status(201)
+                    .json(user)
+            })
+            .catch(next)
+    } else if (response === false){
+        console.log('FALSE')
+        return res
+            .status(200)
+            .json({
+                errorMessage: "It looks like that isn\'t a valid email address, please check that it is correct and try again."
+            })
+    } else {
+        console.log('ERROR')
+        return res
+            .status(500)
+            .json({errorMessage: 'Internal error'})
+    };
+})
 
-// app.post('/login', jsonParser, (req, res, next) => {
-//     const knexInstance = req.app.get('db')
-//     const {email, password} = req.body;
+app.post('/login', jsonParser, (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    const {email, password} = req.body;
 
-//     EndpointsService.getUserById(knexInstance, email, password)
-//         .then(user => {
-//             if(!user) {
-//                 res
-//                     .status(204)
-//                     .json({error: "User not found."})
-//                     .end()
-//             }
-//             res
-//                 .status(200)
-//                 .json({user: user})
-//                 .end()
-//         })
-//         .catch(next)
-// })
+    EndpointsService.getUserById(knexInstance, email, password)
+        .then(user => {
+            if(!user) {
+                res
+                    .status(204)
+                    .json({error: "User not found."})
+                    .end()
+            }
+            res
+                .status(200)
+                .json({user: user})
+                .end()
+        })
+        .catch(next)
+})
 
-// app.get('/users', (req, res, next) => {
-//     const knexInstance = req.app.get('db')
-//     EndpointsService.getAllUsers(knexInstance)
-//         .then(users =>
-//             res.json(users)
-//         )
-//         .catch(next)
-// })
+app.get('/users', (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    EndpointsService.getAllUsers(knexInstance)
+        .then(users =>
+            res.json(users)
+        )
+        .catch(next)
+})
 
 app.get('/users', (req, res, next) => {
     const knexInstance = db
@@ -157,10 +157,6 @@ app.get('/users', (req, res, next) => {
 app.get('/', (req, res) => {
     res.send('Hello, world!')
 });
-
-
-
-
 
 app.use(function errorHandler(error, req, res, next) {
     let response;
